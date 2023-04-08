c573ca086d6188a753128396817eb6d35e27216d
StecBartlomiej <stecb@student.agh.edu.pl>
2023-04-08 15:45:19 +0200

    Some progress on decoding txt file

--- src/BEncoding.cpp
+++ src/BEncoding.cpp
@@ -1,1 +1,83 @@
 #include "BEncoding.hpp"
+
+using namespace BitTorrent::BEncoding;
+
+namespace
+{
+    char dictionaryStart = 'd';
+    char listStart = 'l';
+    char integerStart = 'i';
+    char stringDivider = ':';
+    char typeEnd = 'e';
+}
+
+TextFile::TextFile(const std::filesystem::path &path): file_{path.string()}
+{
+    /// TODO - improve error logging
+    if (!file_.is_open())
+        printf("Error: cannot open file from path: %s", path.string().c_str());
+}
+
+TextFile::~TextFile()
+{
+    file_.close();
+}
+
+char TextFile::getNextChar()
+{
+    char ch;
+    file_.get(ch);
+    return ch;
+}
+
+std::map<std::string, Object> BitTorrent::BEncoding::Decode(std::filesystem::path &path)
+{
+    TextFile file(path);
+    std::map<std::string, Object> dictionary;
+    char ch;
+}
+
+int32_t BitTorrent::BEncoding::DecodeInteger(TextFile &file)
+{
+    std::ostringstream oss;
+    for (char ch = file.getNextChar(); ch != typeEnd; ch = file.getNextChar())
+        oss << ch;
+    int32_t num = std::stoi(oss.str());
+    return num;
+}
+
+std::string BitTorrent::BEncoding::DecodeString(TextFile &file, int length)
+{
+    std::ostringstream oss;
+    for (int i = 0; i < length; ++i)
+    {
+        oss << file.getNextChar();
+    }
+    return oss.str();
+}
+
+std::map<std::string, Object> BitTorrent::BEncoding::DecodeDictionary(TextFile &file)
+{
+    std::map<std::string, Object> dictionary;
+    char ch = file.getNextChar();
+    while (ch != typeEnd)
+    {
+        std::string key;
+        if (isdigit(ch))
+        {
+            std::ostringstream oss;
+            oss << ch;
+            for (char c = file.getNextChar(); c != stringDivider; c = file.getNextChar())
+            {
+                oss << c;
+            }
+            key = DecodeString(file, std::stoi(oss.str()));
+        }
+
+        Object value;
+        if (ch == integerStart)
+            value.data = DecodeInteger(file);
+//        else if (isdigit(ch))
+//            value.data = DecodeString(file)
+    }
+}
--- src/BEncoding.hpp
+++ src/BEncoding.hpp
@@ -1,4 +1,37 @@
 #ifndef BITCLIENT_BENCODING_HPP
 #define BITCLIENT_BENCODING_HPP
 
+#include <filesystem>
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <variant>
+
+namespace BitTorrent::BEncoding
+{
+    /// TODO - add option to write/read
+    class TextFile
+    {
+    public:
+        TextFile(const std::filesystem::path &path);
+        ~TextFile();
+
+        char getNextChar();
+    private:
+        std::ifstream file_;
+    };
+
+    struct Object
+    {
+        std::variant<std::string, int32_t, std::map<std::string, Object>, std::vector<Object>> data;
+    };
+
+    std::map<std::string, Object> Decode(std::filesystem::path &path);
+
+    std::map<std::string, Object> DecodeDictionary(TextFile &file);
+    int32_t DecodeInteger(TextFile &file);
+    std::string DecodeString(TextFile &file, int length);
+}
+
 #endif //BITCLIENT_BENCODING_HPP
