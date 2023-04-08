e0d7e513fc69360fc89d639653219ba7a4311273
StecBartlomiej <stecb@student.agh.edu.pl>
2023-04-08 13:02:00 +0200

    Init commit

---
+++ src/Torrent.hpp
@@ -0,0 +1,35 @@
+#ifndef BITCLIENT_TORRENT_HPP
+#define BITCLIENT_TORRENT_HPP
+
+#include <string>
+#include <vector>
+
+
+namespace BitTorrent
+{
+    struct Tracker
+    {
+        std::string address;
+    };
+
+    struct FileInfo
+    {
+        uint32_t piece_length;
+        std::string pieces;
+        int length;
+        std::string name;
+        bool isPrivate;
+    };
+
+    struct Torrent
+    {
+        std::vector<Tracker> announce;
+        std::string comment;
+        std::string createdBy;
+        std::string creationDate;
+        std::vector<FileInfo> filesInfo;
+        std::string encoding;
+    };
+}
+
+#endif //BITCLIENT_TORRENT_HPP
