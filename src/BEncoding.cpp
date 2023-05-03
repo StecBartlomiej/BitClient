c813634a4d8bd27c57546b172956af4a5794d4d9
StecBartlomiej <stecb@student.agh.edu.pl>
2023-05-03 17:50:49 +0200

    Revert changes of BEncoding and changed to C++20

--- src/BEncoding.cpp
+++ src/BEncoding.cpp
@@ -33,85 +33,88 @@ std::ostream &operator<<(TextFile &file, std::vector<std::string> &vector)
 }
 
 
-//std::vector<std::string> Decoder::Decode()
-//{
-//    std::vector<std::string> vector;
-//
-//    DecodeInternal(vector);
-//
-//    return vector;
-//}
-//
-//void Decoder::DecodeInternal(std::vector<std::string> &vector)
-//{
-//    char ch = file_.GetNextChar();
-//    while (ch)
-//    {
-//        if (ch == 'i')
-//        {
-//            vector.emplace_back("i");
-//            std::ostringstream oss;
-//            for (char c = file_.GetNextChar(); c != 'e'; c = file_.GetNextChar())
-//                oss << c;
-//            vector.emplace_back(oss.str());
-//        }
-//        else if (ch == 'l')
-//        {
-//            vector.emplace_back("l");
-//            for (char c = file_.GetNextChar(); c != 'e' && c != file_.Eof(); c = file_.GetNextChar())
-//            {
-//                file_.Move(-1);
-//                DecodeInternal(vector);
-//            }
-//        }
-//        else if (ch == 'd')
-//        {
-//            vector.emplace_back("d");
-//            for (char c = file_.GetNextChar(); c != 'e' && c != file_.Eof(); c = file_.GetNextChar())
-//            {
-//                file_.Move(-1);
-//                DecodeInternal(vector);
-//            }
-//        }
-//        else if (std::isdigit(ch))
-//        {
-//            vector.emplace_back("s");
-//            std::ostringstream oss;
-//            for (char c = ch; c != ':'; c = file_.GetNextChar())
-//                oss << c;
-//
-//            int length = std::stoi(oss.str());
-//            oss.str("");
-//            oss.clear();
-//
-//            uint8_t byte;
-//            std::size_t count = 0;
-//            while (count <= length)
-//            {
-//                file_.file.read(reinterpret_cast<char*>(&byte), sizeof(byte));
-//                if (!(byte & (1<<7)) || (byte & (1<<6))) // if not 0b10
-//                {
-//                    ++count;
-//                }
-//                if (count > length)
-//                {
-//                    file_.Move(-1);
-//                    break;
-//                }
-//                oss << byte;
-//            }
-//            vector.emplace_back(oss.str());
-//        }
-//        else if (ch == 'e')
-//        {
-//            vector.emplace_back("e");
-//            return;
-//        }
-//        else
-//        {
-//            LOG_ERROR("Decode: bad type in BEncoding");
-//            return;
-//        }
-//        ch = file_.GetNextChar();
-//    }
-//}
+std::vector<std::string> Decoder::Decode()
+{
+    std::vector<std::string> vector;
+
+    DecodeInternal(vector);
+
+//    for (std::string_view x: vector)
+//        std::cout << x << std::endl;
+
+    return vector;
+}
+
+void Decoder::DecodeInternal(std::vector<std::string> &vector)
+{
+    char ch = file_.GetNextChar();
+    while (ch)
+    {
+        if (ch == 'i')
+        {
+            vector.emplace_back("i");
+            std::ostringstream oss;
+            for (char c = file_.GetNextChar(); c != 'e'; c = file_.GetNextChar())
+                oss << c;
+            vector.emplace_back(oss.str());
+        }
+        else if (ch == 'l')
+        {
+            vector.emplace_back("l");
+            for (char c = file_.GetNextChar(); c != 'e' && c != file_.Eof(); c = file_.GetNextChar())
+            {
+                file_.Move(-1);
+                DecodeInternal(vector);
+            }
+        }
+        else if (ch == 'd')
+        {
+            vector.emplace_back("d");
+            for (char c = file_.GetNextChar(); c != 'e' && c != file_.Eof(); c = file_.GetNextChar())
+            {
+                file_.Move(-1);
+                DecodeInternal(vector);
+            }
+        }
+        else if (std::isdigit(ch))
+        {
+            vector.emplace_back("s");
+            std::ostringstream oss;
+            for (char c = ch; c != ':'; c = file_.GetNextChar())
+                oss << c;
+
+            int length = std::stoi(oss.str());
+            oss.str("");
+            oss.clear();
+
+            uint8_t byte;
+            std::size_t count = 0;
+            while (count <= length)
+            {
+                file_.file.read(reinterpret_cast<char*>(&byte), sizeof(byte));
+                if (!(byte & (1<<7)) || (byte & (1<<6))) // if not 0b10
+                {
+                    ++count;
+                }
+                if (count > length)
+                {
+                    file_.Move(-1);
+                    break;
+                }
+                oss << byte;
+            }
+            vector.emplace_back(oss.str());
+        }
+        else if (ch == 'e')
+        {
+            vector.emplace_back("e");
+            return;
+        }
+        else
+        {
+            LOG_ERROR("Decode: bad type in BEncoding");
+            return;
+        }
+        ch = file_.GetNextChar();
+    }
+}
--- src/BEncoding.hpp
+++ src/BEncoding.hpp
@@ -1,9 +1,6 @@
 #ifndef BITCLIENT_BENCODING_HPP
 #define BITCLIENT_BENCODING_HPP
 
-#include "BEncodingType.hpp"
-#include "Log.hpp"
-
 #include <filesystem>
 #include <string>
 #include <map>
@@ -34,12 +31,9 @@ namespace BitTorrent
     public:
         explicit Decoder(const std::filesystem::path &path): file_{path} {}
 
-        template<typename T>
-        constexpr auto Decode() -> DataDictionary<T>;
+        std::vector<std::string> Decode();
     private:
-        template<typename T>
-        void DecodeInternal(DataDictionary<T> dataDictionary);
-
+        void DecodeInternal(std::vector<std::string> &vector);
     private:
         TextFile file_;
     };
@@ -56,87 +50,6 @@ namespace BitTorrent
     };
 
 
-    template<typename T>
-    constexpr auto Decoder::Decode() -> DataDictionary<T>
-    {
-        DataDictionary<T> dataDictionary;
-        DecodeInternal(dataDictionary);
-        return dataDictionary;
-    }
-
-    template<typename T>
-    void Decoder::DecodeInternal(DataDictionary<T> dataDictionary)
-    {
-        char ch = file_.GetNextChar();
-        while (ch)
-        {
-            if (ch == 'i')
-            {
-                std::ostringstream oss;
-                for (char c = file_.GetNextChar(); c != 'e'; c = file_.GetNextChar())
-                    oss << c;
-
-            }
-            else if (ch == 'l')
-            {
-                vector.emplace_back("l");
-                for (char c = file_.GetNextChar(); c != 'e' && c != file_.Eof(); c = file_.GetNextChar())
-                {
-                    file_.Move(-1);
-                    DecodeInternal(vector);
-                }
-            }
-            else if (ch == 'd')
-            {
-                vector.emplace_back("d");
-                for (char c = file_.GetNextChar(); c != 'e' && c != file_.Eof(); c = file_.GetNextChar())
-                {
-                    file_.Move(-1);
-                    DecodeInternal(vector);
-                }
-            }
-            else if (std::isdigit(ch))
-            {
-                vector.emplace_back("s");
-                std::ostringstream oss;
-                for (char c = ch; c != ':'; c = file_.GetNextChar())
-                    oss << c;
-
-                int length = std::stoi(oss.str());
-                oss.str("");
-                oss.clear();
-
-                uint8_t byte;
-                std::size_t count = 0;
-                while (count <= length)
-                {
-                    file_.file.read(reinterpret_cast<char*>(&byte), sizeof(byte));
-                    if (!(byte & (1<<7)) || (byte & (1<<6))) // if not 0b10
-                    {
-                        ++count;
-                    }
-                    if (count > length)
-                    {
-                        file_.Move(-1);
-                        break;
-                    }
-                    oss << byte;
-                }
-                vector.emplace_back(oss.str());
-            }
-            else if (ch == 'e')
-            {
-                vector.emplace_back("e");
-                return;
-            }
-            else
-            {
-                LOG_ERROR("Decode: bad type in BEncoding");
-                return;
-            }
-            ch = file_.GetNextChar();
-        }
-    }
 }
 
 #endif //BITCLIENT_BENCODING_HPP
