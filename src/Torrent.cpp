d353e6e336d9931aaa3cae935bc35b1343e33ba3
StecBartlomiej <stecb@student.agh.edu.pl>
2023-05-03 17:31:59 +0200

    Start of implementing torrent

--- src/Torrent.cpp
+++ src/Torrent.cpp
@@ -1,1 +1,21 @@
 #include "Torrent.hpp"
+
+//BitTorrent::Torrent::Torrent(std::string name, std::string location, std::vector<FileItem> files,
+//                     std::vector<std::string> trackers, uint32_t pieceSize, std::string pieceHashes,
+//                     uint32_t blockSize, bool isPrivate)
+//                     : name{std::move(name)}, location{std::move(location)}, files{std::move(files)},
+//                     pieceHashes{std::move(pieceHashes)}, isPrivate{isPrivate}
+//{
+//
+//}
+BitTorrent::Torrent BitTorrent::GetTorrent(const std::filesystem::path &path)
+{
+    Decoder decoder(path);
+    std::vector<std::string> file_param = decoder.Decode();
+    Torrent torrent;
+
+    for (auto idx = file_param.begin(); idx != file_param.end(); ++idx)
+    {
+
+    }
+}
--- src/Torrent.hpp
+++ src/Torrent.hpp
@@ -1,6 +1,8 @@
 #ifndef BITCLIENT_TORRENT_HPP
 #define BITCLIENT_TORRENT_HPP
 
+#include "BEncoding.hpp"
+
 #include <string>
 #include <vector>
 #include <cinttypes>
@@ -12,24 +14,43 @@ namespace BitTorrent
         std::string address;
     };
 
-    struct FileInfo
+    struct FileItem
     {
-        uint32_t piece_length;
-        std::string pieces;
-        int length;
-        std::string name;
-        bool isPrivate;
+        std::string path;
+        uint32_t size;
+        uint32_t offset;
     };
 
-    struct Torrent
+    class Torrent
     {
-        std::vector<Tracker> announce;
+    public:
+        Torrent() = default;
+        Torrent(std::string name,  std::string location, std::vector<FileItem> files, std::vector<std::string> trackers,
+                uint32_t pieceSize, std::string pieceHashes = "", uint32_t blockSize = 16384u, bool isPrivate = false);
+
+
+    public:
+        std::string name;
+        bool isPrivate;
+        std::vector<FileItem> files;
+
+        std::string fileDirectory;
+        std::string downloadDirectory;
+
+        std::vector<Tracker> trackers;
         std::string comment;
         std::string createdBy;
         std::string creationDate;
-        std::vector<FileInfo> filesInfo;
-        std::string encoding;
+
+        // SHA1 20 UTF8 char for each piece
+        std::vector<std::string> pieceHashes;
+
+        uint32_t blockSize;
+        uint32_t pieceSize;
+        uint32_t totalSize;
     };
+
+    [[nodiscard]] Torrent GetTorrent(const std::filesystem::path &path);
 }
 
 #endif //BITCLIENT_TORRENT_HPP
